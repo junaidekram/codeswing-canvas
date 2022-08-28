42092ad2a0ce2d4b4f77fe0645287c86fe319c78
junaidekram <112276644+junaidekram@users.noreply.github.com>
2022-08-28 14:58:52 -0500

    test

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const c = document.getElementById('canvas').getContext('2d')
 
-c.fillStyle = 'black'
-c.strokeRect(0, 0, 500, 400)
+c.fillStyle = 'black';
+c.strokeRect(0, 0, 500, 400);
 
-c.fillStyle = 'blue'
-c.fillRect(10, 10, 480, 380)
+c.fillStyle = 'black';
+c.fillRect(0, 0, 500, 400);
 
-c.fillStyle = 'orange'
-c.font = 'small-caps bold 24px sans-serif'
-c.textAlign = 'center'
-c.fillText('Hello code hackerz!', 250, 200)
+c.fillStyle = "grey";
+var n = 1600;
+var size = 20;
+var w = c.canvas.width;
+var h = c.canvas.height;
+
+var starx = (w - size) * Math.random();
+var stary = (h - size) * Math.random();
+fillStar(starx, stary, size);
+c.fillStyle = "blue"
+
+for (var i = 0; i < n; i = i + 1) {
+  var x = (w - size) * Math.random();
+  var y = (h - size) * Math.random();
+  fillStar(x, y, size);
+}
+
+var giveup = document.getElementbyId("one")
+
+giveup.onclick = function() {
+  c.fillStyle("gray")
+  fillStar(starx, stary, size)
+}
+
+function fillStar(x, y, s) {
+  c.beginPath();
+  c.moveTo(x, y + s * 0.4);
+  c.lineTo(x + s, y + s * 0.4);
+  c.lineTo(x + s * 0.15, y + s * 0.9);
+  c.lineTo(x + s / 2, y);
+  c.lineTo(x + s * 0.85, y + s * 0.9);
+  c.lineTo(x, y + s * 0.4);
+  c.fill();
+}
