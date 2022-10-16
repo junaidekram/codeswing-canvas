4ee4cc2010ee501bd575e2c3bcb08b337bf10339
junaidekram <112276644+junaidekram@users.noreply.github.com>
2022-10-16 12:27:42 -0500

    Edited the reset button. Made answer button work.

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 const c = document.getElementById('canvas').getContext('2d')
 
-c.fillStyle = 'black';
-c.strokeRect(0, 0, 500, 400);
-
-c.fillStyle = 'black';
-c.fillRect(0, 0, 500, 400);
-
-c.fillStyle = "grey";
 var n = 1600;
 var size = 20;
 var w = c.canvas.width;
 var h = c.canvas.height;
 
-var starx = (w - size) * Math.random();
-var stary = (h - size) * Math.random();
-fillStar(starx, stary, size);
-c.fillStyle = "blue"
+var starx;
+var stary;
 
-for (var i = 0; i < n; i = i + 1) {
-  var x = (w - size) * Math.random();
-  var y = (h - size) * Math.random();
-  fillStar(x, y, size);
+function init() {
+  c.fillStyle = 'black';
+  c.strokeRect(0, 0, 500, 400);
+  
+  c.fillStyle = 'black';
+  c.fillRect(0, 0, 500, 400);
+  
+  c.fillStyle = "grey";
+  starx = (w - size) * Math.random();
+  stary = (h - size) * Math.random();
+  fillStar(starx, stary, size);
+  c.fillStyle = "blue"
+  
+  for (var i = 0; i < n; i = i + 1) {
+    var x = (w - size) * Math.random();
+    var y = (h - size) * Math.random();
+    fillStar(x, y, size);
+  }  
 }
 
-var giveup = document.getElementbyId("one")
-
-giveup.onclick = function() {
-  c.fillStyle("gray")
-  fillStar(starx, stary, size)
-}
+init()
 
 function fillStar(x, y, s) {
   c.beginPath();
@@ -39,4 +39,17 @@ function fillStar(x, y, s) {
   c.lineTo(x + s * 0.85, y + s * 0.9);
   c.lineTo(x, y + s * 0.4);
   c.fill();
+}
+
+var reset = document.getElementById("resetButton")
+var giveup = document.getElementById("answerButton")
+
+giveup.onclick = function() {
+  //alert(`giveup! ${starx},${stary}`)
+  c.fillStyle = "gray"
+  fillStar(starx, stary, size)
+}
+
+reset.onclick = function () {
+init()
 }
